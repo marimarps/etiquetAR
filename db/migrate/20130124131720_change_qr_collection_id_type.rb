93b759f91d2da3bfbe172b1b788a7aa6daa1c985
Alejandro Martínez <yosoyzenitram@gmail.com>
2013-01-24 16:19:49 +0100

    migration collections

--- db/migrate/20130124131720_change_qr_collection_id_type.rb
+++ db/migrate/20130124131720_change_qr_collection_id_type.rb
@@ -1,5 +1,14 @@
 class ChangeQrCollectionIdType < ActiveRecord::Migration
-  def change
-  	change_column :qrs, :collection_id, :integer
+  def up
+  	add_column :qrs, :collection_id, :integer
+  	add_index :qrs, :collection_id
+  	remove_column :qrs, :collection_id
   end
+
+  def down
+  	add_column :qrs, :collection_id, :string
+  	remove_column :qrs, :collection_id
+  end
+end
+
 end
